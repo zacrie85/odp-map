960f5bbd745efd7d59586687740cb0c8de51d2cb
zacrie85 <zacrie85@gmail.com>
2026-08-09 00:02:54 +0700

    Add files via upload

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,8 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  output: "standalone",
+  /* config options here */
   typescript: {
     ignoreBuildErrors: true,
   },
--- src/lib/db.ts
+++ src/lib/db.ts
@@ -6,6 +6,8 @@ const globalForPrisma = globalThis as unknown as {
 
 export const db =
   globalForPrisma.prisma ??
-  new PrismaClient()
+  new PrismaClient({
+    log: ['query'],
+  })
 
 if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = db
