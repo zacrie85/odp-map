ca3d521f6e3d0927cd596b2f45b6dd6896aee0f4
zacrie85 <zacrie85@gmail.com>
2026-08-09 12:36:24 +0700

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,12 +1,1 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
-  output: "standalone",
-  /* config options here */
-  typescript: {
-    ignoreBuildErrors: true,
-  },
-  reactStrictMode: false,
-};
-
-export default nextConfig;
+"fix config"
